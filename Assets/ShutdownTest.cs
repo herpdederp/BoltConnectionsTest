f9268e75cbad515ecbafbffd511adc903c17f684
Stanchion <callumgolds@hotmail.com>
2018-11-08 16:27:29 -0800

    init

---
+++ Assets/ShutdownTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Bolt;
+using UdpKit;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ShutdownTest : Bolt.GlobalEventListener
+{
+
+    public override void BoltShutdownBegin(AddCallback registerDoneCallback)
+    {
+        registerDoneCallback(Test0);
+    }
+
+    void Test0()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public override void ConnectRequest(UdpEndPoint endpoint, IProtocolToken token)
+    {
+        BoltNetwork.Accept(endpoint);
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(BoltNetwork.connections);
+        Debug.Log(BoltNetwork.clients);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            BoltLauncher.Shutdown();
+        }
+    }
+}
---
+++ Assets/samples/BoltInit.cs
@@ -0,0 +1,175 @@
+using UnityEngine;
+using System;
+using UdpKit;
+using UnityEngine.SceneManagement;
+using udpkit.platform.photon.photon;
+
+namespace Bolt.Samples
+{
+    public class UserToken : IProtocolToken
+    {
+
+        public string username;
+
+        public UserToken(string username)
+        {
+            this.username = username;
+        }
+
+        public UserToken()
+        {
+            this.username = "";
+        }
+
+        public void Write(UdpPacket packet)
+        {
+            packet.WriteString(username);
+        }
+
+        public void Read(UdpPacket packet)
+        {
+            username = packet.ReadString();
+        }
+    }
+
+    public class BoltInit : Bolt.GlobalEventListener
+    {
+        public override void BoltStartBegin() // Line 20 on PhotonInit.cs
+        {
+            BoltNetwork.RegisterTokenClass<UserToken>();
+        }
+
+        enum State
+        {
+            SelectMode,
+            SelectMap,
+            SelectRoom,
+            StartServer,
+            StartClient,
+            Started,
+        }
+
+        State state;
+        string map;
+
+        void OnGUI()
+        {
+            Rect tex = new Rect(10, 10, 140, 75);
+            Rect area = new Rect(10, 90, Screen.width - 20, Screen.height - 100);
+
+            GUI.Box(tex, Resources.Load("BoltLogo") as Texture2D);
+            GUILayout.BeginArea(area);
+
+            switch (state)
+            {
+                case State.SelectMode: State_SelectMode(); break;
+                case State.SelectMap: State_SelectMap(); break;
+                case State.SelectRoom: State_SelectRoom(); break;
+                case State.StartClient: State_StartClient(); break;
+                case State.StartServer: State_StartServer(); break;
+            }
+
+            GUILayout.EndArea();
+        }
+
+        void State_SelectRoom()
+        {
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label("Looking for rooms:");
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginVertical();
+
+            foreach (var session in BoltNetwork.SessionList)
+            {
+                var photonSession = session.Value as PhotonSession;
+
+                if (photonSession.Source == UdpSessionSource.Photon)
+                {
+                    var matchName = photonSession.HostName;
+                    var label = string.Format("Join: {0} | {1}/{2}", matchName, photonSession.ConnectionsCurrent, photonSession.ConnectionsMax);
+
+                    if (ExpandButton(label))
+                    {
+                        UserToken connectToken = new UserToken
+                        {
+                            username = "ConnectTokenData"
+                        };
+
+                        BoltNetwork.Connect(photonSession, connectToken);
+                        state = State.Started;
+                    }
+                }
+            }
+
+            GUILayout.EndVertical();
+        }
+
+        void State_SelectMode()
+        {
+            if (ExpandButton("Server"))
+            {
+                state = State.SelectMap;
+            }
+            if (ExpandButton("Client"))
+            {
+                state = State.StartClient;
+            }
+        }
+
+        void State_SelectMap()
+        {
+            GUILayout.BeginVertical();
+
+            foreach (string value in BoltScenes.AllScenes)
+            {
+                if (SceneManager.GetActiveScene().name != value)
+                {
+                    if (ExpandButton(value))
+                    {
+                        map = value;
+                        state = State.StartServer;
+                    }
+                }
+            }
+
+            GUILayout.EndVertical();
+        }
+
+        void State_StartServer()
+        {
+            BoltLauncher.StartServer();
+            state = State.Started;
+        }
+
+        void State_StartClient()
+        {
+            BoltLauncher.StartClient();
+            state = State.SelectRoom;
+        }
+
+        public override void BoltStartDone()
+        {
+            if (BoltNetwork.isServer)
+            {
+                var id = Guid.NewGuid().ToString().Split('-')[0];
+                var matchName = string.Format("{0} - {1}", id, map);
+
+                BoltNetwork.SetServerInfo(matchName, null);
+                BoltNetwork.LoadScene(map);
+            }
+        }
+
+        bool ExpandButton(string text)
+        {
+            return GUILayout.Button(text, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+        }
+
+        public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+        {
+            BoltLog.Info("New session list");
+        }
+    }
+}
