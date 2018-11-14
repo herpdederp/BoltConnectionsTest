1560cdc56ebeba4a135051513e015904896caa3e
Stanchion <callumgolds@hotmail.com>
2018-11-14 03:44:23 -0800

    Added CallbackTest.cs

---
+++ Assets/CallbackTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallbackTest : Bolt.GlobalEventListener
+{
+
+    public override void SceneLoadLocalDone(string map)
+    {
+        Debug.Log("SceneLoadLocalDone");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
